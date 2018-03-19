acc27198997fb0f04fcfd5e3dc4ecd5a2db0e52e
Ammar <ammar.mian08@gmail.com>
2018-03-19 08:48:09 -0400

    serial: json echo server

---
+++ 02-serialization/05-b-serial-json-echo-server.go
@@ -0,0 +1,75 @@
+/*
+  Echo Server + JSON (SERVER)
+*/
+
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net"
+	"os"
+)
+
+type Person struct {
+	Name  Name
+	Email []Email
+}
+
+type Name struct {
+	Family   string
+	Personal string
+}
+
+type Email struct {
+	Kind    string
+	Address string
+}
+
+func (p Person) String() string {
+	s := p.Name.Personal + " " + p.Name.Family
+	for _, v := range p.Email {
+		s += "\n" + v.Kind + ": " + v.Address
+	}
+	return s
+}
+
+func main() {
+	service := ":1200"
+
+	tcpAddr, err := net.ResolveTCPAddr("tcp", service)
+	checkError(err)
+
+	listener, err := net.ListenTCP("tcp", tcpAddr)
+	checkError(err)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			continue
+		}
+
+		// net.Conn -> io.Reader and io.Writer
+		encoder := json.NewEncoder(conn)
+		decoder := json.NewDecoder(conn)
+
+    // do it 10 times!
+		for n := 0; n < 10; n++ {
+			var person Person
+
+			// read from conn
+			decoder.Decode(&person)
+
+			// write to conn
+			encoder.Encode(person)
+		}
+		conn.Close()
+	}
+}
+
+func checkError(err error) {
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Fatal error %s", err.Error())
+		os.Exit(1)
+	}
+}
